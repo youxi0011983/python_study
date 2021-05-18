84983274abad7f59bc78e68a9d05ab3ac7a6c375
iron <youxi0011983@163.com>
2021-05-18 15:52:19 +0800

    change Deletecolumns.py, Can read excel file and change column to save to excel file.

--- excel/Deletecolumns.py
+++ excel/Deletecolumns.py
@@ -1,2 +1,8 @@
-if __name__ == '__main__':
-    print('hello world!')
+import pandas as pd
+
+df = pd.read_excel('all_sales.xlsx')
+
+# 读所有行的spu以及level1列的值，这里需要嵌套列表
+df_delete_column = df.drop(labels='spu', axis=1)
+
+df_delete_column.to_excel('new.xlsx')
